30c9abf2e7eb3511b6ee6092b87c038627fb2d73
Subhadeep701 <73311586+Subhadeep701@users.noreply.github.com>
2025-05-19 12:18:28 +0530

    Update swap_test.py
    A stateprepartion is used

--- swap_test.py
+++ swap_test.py
@@ -1,18 +1,24 @@
 from qiskit import QuantumCircuit
-from qiskit.primitives import StatevectorEstimator
-from qiskit.quantum_info import Operator,Statevector
+from qiskit.circuit.library import StatePreparation
+from qiskit.quantum_info import Statevector
 import numpy as np
+from matplotlib import pyplot as plt
 qc=QuantumCircuit(3)
 qc.h(0)
-qc.x(1)
-qc.h(1)
-qc.x(2)
-qc.h(2)
+print(f"{(lambda x: x / np.linalg.norm(x))(np.random.randn(2))}")
+v=(lambda x:x/np.linalg.norm(x))(np.random.randn(2))
+w=(lambda x:x/np.linalg.norm(x))(np.random.randn(2))
+###in the second qubit put the statevector w
+# to see the effect that 0 state probability is not 1
+qc.append(StatePreparation(v),qargs=[1])
+qc.append(StatePreparation(v),qargs=[2])
+#qc.append(StatePreparation(w),qargs=[2])
 qc.cswap(0,1,2)
 qc.h(0)
+qc.draw("mpl")
 b=Statevector(qc).sample_counts(qargs=[0],shots=1000)
 print(b)
 count_0=sum(value for key,value in b.items() if key.startswith("0"))
 print(f"Probability of 0: {count_0/sum(b.values())}")
-
+plt.show()
 
